68b116091a24c52925bdc1e291f79428540ded63
DiogoMMarto <diogo.marto@ua.pt>
2025-06-15 23:23:41 +0100

    Fix configuration loading and improve broker IP handling in config.py

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,13 +1,14 @@
-from yaml_loader import load_yaml
+from utils.yaml_loader import load_yaml
 import os
 import dotenv
-# Load environment variables from .env file
 dotenv.load_dotenv()
 
 CONFIG = load_yaml("config/workstation_config.yaml")
+print(f"[CONFIG] Loaded configuration: {CONFIG}")
 
-broker_ip_os = os.getenv("BROKER_IP", CONFIG["broker_ip"])
-CONFIG["broker_ip"] = broker_ip_os
+broker_ip_os = os.getenv("BROKER_IP", CONFIG["mqtt"].get("broker_ip", "localhost"))
+CONFIG["mqtt"]["broker_ip"] = broker_ip_os
+print(f"[CONFIG] Broker IP set to: {broker_ip_os}")
 
 
 
